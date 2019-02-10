58da60d50f1d9a89babe054f9d3b5b7dfb3a6821
Pedro Morales <pedromorales@MacBook-Air-de-Pedro.local>
2019-02-10 03:52:38 -0300

    Creacion del proyecto

---
+++ ContactApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactApp
+//
+//  Created by Pedro Morales on 2/10/19.
+//  Copyright © 2019 PanaceaSpa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
